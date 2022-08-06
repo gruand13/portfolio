7fa941483fbee0212ad0a491196f8a8bbf4cefd5
Andrey Hrusheuski <gruand13@gmail.com>
2022-08-06 18:20:18 +0300

    add block skills and small js function for skills-ratings bar

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', ()=>{
 close.addEventListener('click', ()=>{
     menu.classList.remove('active');
 });
+
+const counter = document.querySelectorAll('.skills__ratings-counter'),
+        lines = document.querySelectorAll('.skills__ratings-line span');
+
+    counter.forEach((item, i) =>{
+        lines[i].style.width = item.innerHTML;
+    });
